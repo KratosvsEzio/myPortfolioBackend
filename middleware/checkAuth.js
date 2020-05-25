b9f970610c9d40f3705fdc7a5a1ff374a0e7c07a
KratosvsEzio <45562464+KratosvsEzio@users.noreply.github.com>
2020-05-25 05:39:35 +0500

    Update check middleware

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,10 +1,13 @@
 const {OAuth2Client} = require('google-auth-library');
-
+const CLIENT_ID = '5126203523-o4445m07k6m6kjj9313p8flpd2meqmpd.apps.googleusercontent.com';
 module.exports = (req, res, next) => {
     console.log(req.headers.authorization);
     try{
         const token = req.headers.authorization.split(" ")[1];
         console.log(token);
+        console.log('Next');
+        console.log('Next');
+        console.log('Next');
         if (token !== 'null') {
             auth = verify(token)
             .then((res) => {
@@ -25,10 +28,10 @@ module.exports = (req, res, next) => {
     }
 }
 async function verify(token) {
-    const client = new OAuth2Client('5126203523-o4445m07k6m6kjj9313p8flpd2meqmpd.apps.googleusercontent.com');
+    const client = new OAuth2Client(CLIENT_ID);
     const ticket = await client.verifyIdToken({
         idToken: token,
-        audience: '55126203523-o4445m07k6m6kjj9313p8flpd2meqmpd.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
     });
